de37366d75708bfada9224238ca4642b64649fec
Bee <thammawat@muze.co.th>
2025-03-25 19:29:30 +0700

    update detail

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -5,9 +5,9 @@ const AppBar = () => {
   const location = useLocation();
   const renderTabText = (pathname: string, text: string) => {
     // eslint-disable-next-line react/jsx-no-undef
-    return <Text fontWeight={location.pathname === pathname ? 'bold' : 'normal'} color={location.pathname === pathname ? '#C0C0C0' : 'white'}>{text}</Text>
+    return <Box bg={location.pathname === pathname ? '#1A69AA' : '#373535'} p={'4'}><Text fontWeight={location.pathname === pathname ? 'bold' : 'normal'}>{text}</Text></Box>
   }
-  return <Box bg='#373535' width="100%" padding="4" color="white" display='flex' alignItems='center'>
+  return <Box bg='#373535' width="100%" color="white" display='flex' alignItems='center'>
     <Box marginRight='20px'>
       <Link to="/upload">{renderTabText('/upload', 'Upload')}</Link>
     </Box>
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,10 +1,12 @@
 import { Box, Button, Input } from "@chakra-ui/react"
 import Logo from '../assets/image/jtekt.png'
 import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 const LoginPage = () => {
   const [username, setUsername] = useState("")
   const [password, setPassowrd] = useState("")
+  const navigate = useNavigate();
   return <Box height={'90vh'} display='flex' alignItems='center' justifyContent='center'>
     <Box shadow={'0 4px 6px -1px rgba(0, 0, 0, .1), 0 2px 4px -2px rgba(0, 0, 8, .1)'} width="400px" padding="20px" border={"1px solid #ECECEC"} borderRadius="10px">
       <Box display={'flex'} justifyContent={'center'}>
@@ -12,7 +14,7 @@ const LoginPage = () => {
       </Box>
       <Input marginTop={"25px"} value={username} onChange={e => { setUsername(e.currentTarget.value) }} />
       <Input marginTop={"25px"} value={password} type="password" onChange={e => { setPassowrd(e.currentTarget.value) }} />
-      <Button  marginTop={"25px"} width="100%">Login</Button>
+      <Button colorPalette={"brand"} marginTop={"25px"} width="100%" onClick={() => { navigate("/upload") }}>Login</Button>
     </Box>
   </Box>
 }
