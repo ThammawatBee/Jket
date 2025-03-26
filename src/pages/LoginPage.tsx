73a77b11bb829476e249b5d072cb5d60876638f0
Bee <thammawat@muze.co.th>
2025-03-26 22:56:16 +0700

    update logo

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,5 @@
 import { Box, Button, Input } from "@chakra-ui/react"
-import Logo from '../assets/image/jtekt.png'
+import Logo from '../assets/image/logo.jpg'
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 
@@ -10,7 +10,7 @@ const LoginPage = () => {
   return <Box height={'90vh'} display='flex' alignItems='center' justifyContent='center'>
     <Box shadow={'0 4px 6px -1px rgba(0, 0, 0, .1), 0 2px 4px -2px rgba(0, 0, 8, .1)'} width="400px" padding="20px" border={"1px solid #ECECEC"} borderRadius="10px">
       <Box display={'flex'} justifyContent={'center'}>
-        <img src={Logo} style={{ height: "120px", width: "120px" }} />
+        <img src={Logo} style={{ height: "125px", width: "125px" }} />
       </Box>
       <Input marginTop={"25px"} value={username} onChange={e => { setUsername(e.currentTarget.value) }} />
       <Input marginTop={"25px"} value={password} type="password" onChange={e => { setPassowrd(e.currentTarget.value) }} />
